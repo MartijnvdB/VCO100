361e6526c63b079044e26e4bd556025ff0907a9b
MartijnvdB <martijn@palebluedot.nl>
2016-04-16 14:48:08 +0200

    Added examples

---
+++ examples/ArduinoExampleSerial/ArduinoExampleSerial.ino
@@ -0,0 +1,139 @@
+
+/*
+   Name:    ArduinoExamplesSerial
+   Date:    April 2016
+   Author:  MBUR
+
+   Exampleh sketch for use with the Voltcraft CO-100 class, VCO100, in the form of
+   a Finite State Machine (FSM).
+
+
+   instanciate the object,
+   wait for the moment that a new data frame has arrived,
+   sample the data frame,
+   output the data as text to the serial console.
+
+*/
+
+
+
+#include <VCO100.h>
+
+#define CLOCK_PIN 2     // clock IN from CO-100
+#define DATA_PIN 3      // data IN from CO-100
+#define ledPin 13       // debug LED
+#define MINWAIT 20000   // uSec
+#define PUBLISH_DELAY_MILLIS 3000  // time between consecutive MQTT publications
+
+
+/* Global variables */
+
+byte prefdirty;
+volatile byte val;              // pin value read in the ISR
+volatile int dirty;             // dirty flag for data processing
+int nextStatus = 0;             // status variable for FSM
+unsigned long beginTime;
+unsigned long publishTime = 0;  // last time values were published to MQTT
+// Measured values:
+float cotwo, temp, hum;
+// Previously measured data values:
+float prefCotwo, prefHum, prefTemp;
+
+
+/* Main sketch */
+
+VCO100 Voltcraft;
+
+void setup() {
+  pinMode(DATA_PIN, INPUT);   // Data out, Voltcraft
+  pinMode(CLOCK_PIN, INPUT);  // Clock out, Voltcraft
+  pinMode(ledPin, OUTPUT);
+
+  attachInterrupt(digitalPinToInterrupt(CLOCK_PIN), ISR_readdata, FALLING);
+
+  Serial.begin(115200);
+  Serial.println("Serial ready.");
+}
+
+
+// loop() implements a FSM using switch/case statements.
+void loop() {
+
+  switch (nextStatus) {
+    case 0: // wait (blocking) for a moment when CLOCK_PIN has been HIGH for > MINWAIT us
+      beginTime = micros();
+      while (digitalRead(CLOCK_PIN)) {
+        if (micros() - beginTime > MINWAIT) {
+          nextStatus = 10;
+          break;
+        } // if
+      }  // while HIGH
+      break;
+    case 10:
+      // Read data: on the FALLING clock read the DATA_PIN.
+      prefdirty = dirty;  // flag voor data change
+      Voltcraft.reset();  // reset internals for next round
+
+      // Read values until we had enough.
+      while ( Voltcraft.readmore() ) {
+        if (prefdirty != dirty) { // new value read by the ISR
+          Voltcraft.store(val);   // store it
+          prefdirty = dirty;
+        }
+      }
+
+      nextStatus = 20;
+      break;
+    case 20:
+      cotwo = Voltcraft.getValue('P');
+      hum = Voltcraft.getValue('A');
+      temp = Voltcraft.getValue('B');
+
+      /* Publish values
+          Only:
+          - if more than PUBLISH_DELAY_MILLIS has passed since previous publication
+          - if data is valid
+          - if data has changed wrt. previous values
+      */
+      if (millis() - publishTime > PUBLISH_DELAY_MILLIS) {
+        if (cotwo && cotwo != prefCotwo) {
+          Serial.print("CO2 [ppm]: ");
+          Serial.println(cotwo);
+          prefCotwo = cotwo;
+          publishTime = millis();
+        }
+        if (hum && hum != prefHum) {
+          Serial.print("Hum [%]: ");
+          Serial.println(hum);
+          prefHum = hum;
+          publishTime = millis();
+        }
+        if (temp && temp != prefTemp) {
+          Serial.print("Temp. [degC]: ");
+          Serial.println(Voltcraft.getValue('1'));
+          prefTemp = temp;
+          publishTime = millis();
+        }
+      }
+      nextStatus = 0;
+      break;
+    default:  // we should never get here
+      digitalWrite(ledPin, HIGH);
+      delay(1000);
+      digitalWrite(ledPin, LOW);
+      break;
+  } // switch
+
+} // loop
+
+
+/*
+     Interrupt service routine, gets called on the FALLING clock.
+     Not placed in the VCO100 class for simplicity's sake.
+*/
+void ISR_readdata() {
+  val = digitalRead(DATA_PIN);
+  dirty = !dirty; // toggle flag
+} // ISD_readdata
+
+
---
+++ examples/OakExampleMQTT/OakExampleMQTT.ino/OakExampleMQTT.ino.ino
@@ -0,0 +1,255 @@
+
+/*
+   Name:    OakExampleMQTT
+   Date:    April 2016
+   Author:  MBUR
+
+   Example sketch for use with the Voltcraft CO-100 class, VCO100, in the form of
+   a Finite State Machine (FSM).
+
+   instantiate the object,
+   wait for the moment that a new data frame has arrived,
+   sample the data frame,
+   output the data wirelessly to an MQTT topic.
+
+*/
+
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <VCO100.h>
+
+
+/* MQTT variables */
+const char* mqtt_server = "192.168.178.190";    // raspberrypi, Domoticz
+const unsigned int mqtt_port = 11883;
+const char* connection_id = "ESP8266Client";
+const char* client_name = "digistumpoak";
+const char* client_password = "yrhft%43";
+const char* outTopic = "domoticz/in";           // MQTT topic for Domoticz
+const char* statusTopic = "outTopic";           // General (debug/status) topic
+
+
+const int domoIdxCO2 = 201;                 // Domoticz device identifier
+const int domoIdxTempHum = 203;             // Domoticz device identifier
+
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+
+/*
+    Oak <-> Voltcraft interface
+*/
+#define CLOCK_PIN 9     // clock IN from CO-100
+#define DATA_PIN 8      // data IN from CO-100
+#define BUILTIN_LED 1   // Oak's LED
+#define MINWAIT_MILLIS 20        // minimum wait time between data words
+#define PUBLISH_DELAY_MILLIS 50  // Forced delay to allow Oak to do it's WiFi stuff
+
+const float tempOffset = 1.0;     // Temp indication on the device seems to be on the high side.
+
+/*
+   Domoticz configuration
+*/
+
+
+/*
+    Global variables
+*/
+int prefdirty;
+volatile int val;                     // pin value read in the ISR
+volatile int dirty;                   // dirty flag for data processing
+volatile unsigned long latestTrigger; // latest time the IST executed
+unsigned long now;                    // timestamp
+int nextStatus = 0;                   // status variable for FSM
+float cotwo, temp, hum;               // measured data values
+float prefCotwo, prefHum, prefTemp;   // previously measured data values
+
+
+
+/*
+   Main sketch
+*/
+
+VCO100 Voltcraft;   // instantiate a VCO100 object
+
+void setup() {
+  pinMode(DATA_PIN, INPUT);   // Data out, Voltcraft
+  pinMode(CLOCK_PIN, INPUT);  // Clock out, Voltcraft
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+
+
+  client.setServer(mqtt_server, mqtt_port);
+
+  attachInterrupt(digitalPinToInterrupt(CLOCK_PIN), ISR_readdata, FALLING);
+
+  // Connect here, in case we want to have some publishing capabilities outside
+  // the publishToMQTT function.
+  reconnect();
+
+}
+
+
+// loop() implements a FSM using switch/case statements.
+void loop() {
+  client.loop();    // MQTT, maintain server connection
+
+//espClient.forceSleepBegin();
+
+  switch (nextStatus) {
+    /* Wait until CLOCK has been HIGH for long enough. */
+    case 0:
+      // When CLOCK is HIGH and has been for more than MINWAIT_MILLIS we continue.
+      // This is like a non-blocking wait state. Do the '>' comparison to capture micros() rollover.
+      now = micros();
+      if (digitalRead(CLOCK_PIN) == HIGH && now > latestTrigger && (now - latestTrigger > MINWAIT_MILLIS) ) {
+        nextStatus = 10;
+        break;
+      }
+
+    /* Reset object for next frame sampling. */
+    case 10:
+      prefdirty = dirty;  // flag voor data change
+      Voltcraft.reset();  // reset internals for next round
+
+      nextStatus = 20;
+      break;
+
+    /* Frame sampling. */
+    case 20:
+      if (Voltcraft.readmore()) {
+        if (prefdirty != dirty) {
+          prefdirty = dirty;
+          Voltcraft.store(val);   // store it
+        }
+      }
+      else {
+        nextStatus = 30;
+      }
+      break;
+
+    /* Formatting and publishing to MQTT. */
+    case 30:
+      char msg[81];
+
+      // Access measured data in object.
+      cotwo = Voltcraft.getValue('P');  // CO2
+      hum = Voltcraft.getValue('A');    // Hum
+      temp = floor(10 * Voltcraft.getValue('1')) / 10;   // Temp, degC, get rid of superfluous decimals
+
+      /* Publish values
+         Only:
+         - if more than PUBLISH_DELAY_MILLIS has passed since previous publication
+         - if data is valid
+         - if data has changed wrt. previous values
+      */
+      if (cotwo && ( abs(cotwo - prefCotwo) >= 5) ) {
+        static char outstr[5];
+        dtostrf(cotwo, sizeof(cotwo), 0, outstr);  // convert float to string
+        snprintf (msg, 50, "{\"name\":\"Voltcraft CO2\",\"idx\":%i,\"nvalue\":%s}", domoIdxCO2, outstr); // format output message
+        publishToMQTT(msg);
+        prefCotwo = cotwo;
+        delay(PUBLISH_DELAY_MILLIS);  // do this to give Oak opportunity to do it's WiFi stuff.
+      }
+      if (hum && temp && ( (hum != prefHum) || ( abs(temp - prefTemp) > 0.15 ) ) ) { // temp. tends to flap...
+        static char outstr[4];
+        int humStatus;  // humidy status
+
+        temp -= tempOffset; // correct for temp. difference.
+
+        // Humidity qualifiers. Specific to Domoticz.
+        // https://www.domoticz.com/wiki/Domoticz_API/JSON_URL's#Retrieve_status_of_specific_device
+        if (hum < 30) {
+          humStatus = 2;  // dry
+        }
+        else if (hum > 70) {
+          humStatus = 3; // wet
+        }
+        else if (hum > 40 && hum < 60) {
+          humStatus = 1;  // confortable
+        }
+        else {
+          humStatus = 0;  // normal
+        }
+        
+        dtostrf(temp, sizeof(temp), 1, outstr);  // convert float to string
+
+        snprintf (msg, 80, "{\"name\":\"Voltcraft Temp en Vocht\",\"idx\":%i,\"nvalue\":0,\"svalue\":\"%s;%i;%i\"}", domoIdxTempHum, outstr, (int)hum, humStatus);
+        publishToMQTT(msg);
+
+        prefHum = hum;
+        prefTemp = temp;
+        delay(PUBLISH_DELAY_MILLIS);
+      }
+
+      nextStatus = 0;
+      break;
+
+    default:  // PANIC - we should never get here!
+      while (1) {
+        digitalWrite(BUILTIN_LED, HIGH);
+        delay(1000);
+        digitalWrite(BUILTIN_LED, LOW);
+        delay(500);
+      }
+  }
+
+  // Allow Oak to handle WiFi stuff in the loop()
+  // while we're notr busy reading data.
+  if (! Voltcraft.readmore() ) {
+    delay(PUBLISH_DELAY_MILLIS);
+  }
+
+} // loop
+
+
+
+/* publish a formatted message to MQTT
+*/
+void publishToMQTT(char* msg) {
+
+  // Reconnect, if necessary.
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.publish(outTopic, msg);
+
+} // publishToMQTT
+
+
+
+/*
+   Reconnect to MQTT
+*/
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect(connection_id, client_name, client_password)) {
+      client.publish(statusTopic, "Oak has (re)connected");
+    } else {
+      // Wait before retrying
+      delay(5000); // msec
+    }
+  }
+} // reconnect
+
+
+
+/*
+   Interrupt service routine, gets called on the FALLING clock.
+   Not placed in the VCO100 class for simplicity's sake.
+*/
+void ISR_readdata() {
+  val = digitalRead(DATA_PIN);
+  dirty = !dirty; // toggle flag
+  latestTrigger = micros(); // this does work inside the ISR.
+} // ISD_readdata
+
+
+
